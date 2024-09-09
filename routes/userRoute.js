51a919a891f1e4c92e49a4a2da568f215f70cfea
JANHVI <shivipandey993@gmail.com>
2024-09-09 20:49:36 +0530

    query model added to useroute.js

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User=require('../models/User')
+const Query=require('../models/query')
 
 router.post("/", async (req, res) => {
   try {
