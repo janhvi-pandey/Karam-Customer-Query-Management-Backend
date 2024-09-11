d5961bc217bff88d9c73cd04ea43c1b206422447
JANHVI <shivipandey993@gmail.com>
2024-09-11 21:54:12 +0530

    user created send as a response

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -6,7 +6,7 @@ const User=require('../models/User')
 router.post("/", async (req, res) => {
   try {
       const result = await User.create(req.body);
-      return res.send({ msg: "Success" });
+      return res.send({ msg: "Success",user:result });
   } catch (error) {
       return res.status(500).send({ msg: "Internal Server Error" });
   }
