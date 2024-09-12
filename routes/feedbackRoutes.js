7ffe479c6b251e51dec3376d9ed4e105da590003
JANHVI <shivipandey993@gmail.com>
2024-09-12 17:20:19 +0530

    log statement in feedback form

--- routes/feedbackRoutes.js
+++ routes/feedbackRoutes.js
@@ -6,13 +6,16 @@ const Feedback = require('../models/Feedback');
 router.post('/submit', async (req, res) => {
   try {
     const { name, email, feedback, attachment } = req.body;
+    console.log(req.body)
     const newFeedback = new Feedback({
       name,
       email,
       feedback,
      
     });
-    await newFeedback.save();
+    const result= await newFeedback.save();
+    console.log(result);
+    
     res.status(201).json({ message: 'Feedback submitted successfully' });
   } catch (error) {
     res.status(500).json({ message: 'Error submitting feedback', error });
